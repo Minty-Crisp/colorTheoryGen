fdf7e0c60e41216c49ee4f8dc1288caddd724b6d
Minty-Crisp <minty-crisp@protonmail.com>
2023-02-09 01:05:00 -0600

    Initial version
    Initial version of colorTheoryGen

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# colorTheoryGen
-Generate a color theory palette from a given color, color family or a random color.
+# Color Theory Generator
+Generate a color theory palette from a given color, random color within a color family or a random color from any family.
+
+---
+[View Examples](https://minty-crisp.github.io/)
+
+---
+
+Colors Generated :
+- Base
+- Complementary
+- Split-Complementary
+- Triadic
+- Tetradic
+- Analagous
+- Monochrome (Coming Soon)
+
+Color Families :
+- Red
+- Orange
+- Yellow
+- Lime
+- Blue
+- Cyan
+- Magenta
+- Maroon
+- Olive
+- Green
+- Purple
+- Teal
+- Navy
+- Silver (Not included in Random selection)
+- Gray (Not included in Random selection)
+- Black (Not included in Random selection)
+- White (Not included in Random selection)
+
+---
+
+## How to Use :
+Use the function `colorTheoryGen()` to return an object containing all colors generated from the provided parameters if any.
+
+Use a Specific Color : (Currently only Hex colors supported)
+
+`let newColor1 = colorTheoryGen('#00d3d3');`
+
+Pick a Random Color from within a Color Family :
+
+`let newColor1 = colorTheoryGen(false, 'red');`
+
+Pick a Random Color from within any Color Family :
+
+`let newColor1 = colorTheoryGen();`
+
+Access the colors from the object with :
+```
+newColor1.base
+newColor1.compl
+newColor1.splitCompl[0]
+newColor1.splitCompl[1]
+newColor1.triadic[0]
+newColor1.triadic[1]
+newColor1.tetradic[0]
+newColor1.tetradic[1]
+newColor1.tetradic[2]
+newColor1.analog[0]
+newColor1.analog[1]
+newColor1.analog[2]
+```
+---
+
+### Future Improvements :
+
+Accept rgb, hsl and text value inputs for the provided color.
+
+Export monochrome color values.
+
+---
+### Minty Crisp ^-^ :watermelon: 
+
+- If you want to see more work of mine, check out my other [repos](https://github.com/Minty-Crisp?tab=repositories) or my site [mintycrisp.com](https://mintycrisp.com).
+- You can also check me out on a few other places like [dev.to](https://dev.to/mintycrisp) or where I spend most of my free time [reddit](https://www.reddit.com/user/Minty-Crisp/).
+- I am available for Web & XR work if you are interested in hiring me or collaborating on future projects.
+- You can reach me via socials or email : minty-crisp @ proton . me
+- If you really enjoy my work and want to support me making more of these types of projects, you can help me out with a [kofi](https://ko-fi.com/mintycrisp) or just by sharing.
---
+++ colorTheoryGen.js
@@ -0,0 +1,321 @@
+//
+//https://github.com/Minty-Crisp/LINK
+//Created by Minty-Crisp (mintycrisp.com)
+//
+//Color Theory Generator
+//Generate a color theory palette from a given color, color family or a random color
+//
+function colorTheoryGen(color, family){
+
+//color accepts Hex values only at the moment, more options coming soon
+
+//Colors Generated :
+//Base
+//Complementary
+//Split-complementary
+//Triadic
+//Tetradic
+//Analagous
+//Monochrome - coming soon
+
+let r;
+let r0;
+let g;
+let g0;
+let b;
+let b0;
+let base;
+let baseRGB;
+let familyCheck = false;
+const colorFamily =['red','orange','yellow','lime','blue','cyan','magenta','maroon','olive','green','purple','teal','navy','silver','grey','black','white'];
+
+//Support Functions
+function HSLToRGB(h,s,l) {
+	// Must be fractions of 1
+	//s /= 100;
+	//l /= 100;
+
+	let c = (1 - Math.abs(2 * l - 1)) * s,
+	  x = c * (1 - Math.abs((h / 60) % 2 - 1)),
+	  m = l - c/2,
+	  r = 0,
+	  g = 0,
+	  b = 0;
+
+	if (0 <= h && h < 60) {
+	r = c; g = x; b = 0;  
+	} else if (60 <= h && h < 120) {
+	r = x; g = c; b = 0;
+	} else if (120 <= h && h < 180) {
+	r = 0; g = c; b = x;
+	} else if (180 <= h && h < 240) {
+	r = 0; g = x; b = c;
+	} else if (240 <= h && h < 300) {
+	r = x; g = 0; b = c;
+	} else if (300 <= h && h < 360) {
+	r = c; g = 0; b = x;
+	}
+	r = Math.round((r + m) * 255);
+	g = Math.round((g + m) * 255);
+	b = Math.round((b + m) * 255);
+
+	return {r,g,b};
+}
+function RGBToHex(r,g,b) {
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+
+	if (r.length == 1)
+	r = "0" + r;
+	if (g.length == 1)
+	g = "0" + g;
+	if (b.length == 1)
+	b = "0" + b;
+
+	return "#" + r + g + b;
+}
+function hexToRGB(h) {
+	let r = 0, g = 0, b = 0;
+
+	// 3 digits
+	if (h.length == 4) {
+	r = "0x" + h[1] + h[1];
+	g = "0x" + h[2] + h[2];
+	b = "0x" + h[3] + h[3];
+
+	// 6 digits
+	} else if (h.length == 7) {
+	r = "0x" + h[1] + h[2];
+	g = "0x" + h[3] + h[4];
+	b = "0x" + h[5] + h[6];
+	}
+
+	//return "rgb("+ +r + "," + +g + "," + +b + ")";
+	return {r,g,b};
+}
+function randomColorFamily(){
+	return colorFamily[Math.floor(Math.random()*(colorFamily.length-4))];
+	//Ignore last 4 Black/White/Grey/Silver
+}
+
+//Check if color input is useable
+if(color){
+	if(color[0] === '#' && color.length === 4 || color[0] === '#' && color.length === 7){} else {
+		color = false;
+	}
+}
+//Check if family input is useable
+if(family){
+	for(let each in colorFamily){
+		if(family === colorFamily[each]){
+			familyCheck = true;
+			break;
+		}
+	}
+	if(familyCheck){} else {
+		family = randomColorFamily();
+	}
+}
+
+//Generate Color Values
+if(color){
+	//color is Hex
+	base = color;
+	baseRGB = hexToRGB(base);
+
+	//convert Hex to RGB
+	r = baseRGB.r;
+	r0 = r/255;
+	g = baseRGB.g;
+	g0 = g/255;
+	b = baseRGB.b;
+	b0 = b/255;
+} else {
+	if(!family){
+		family = randomColorFamily();
+	}
+	if(family === 'red'){
+		r = Math.floor(Math.random()*55)+200;
+		g = b = 0;
+	} else if(family === 'orange'){
+		r = Math.floor(Math.random()*105)+150;
+		g = Math.floor(r*0.65);
+		b = 0;
+	} else if(family === 'yellow'){
+		r = g = Math.floor(Math.random()*55)+200;
+		b = 0;
+	} else if(family === 'lime'){
+		g = Math.floor(Math.random()*55)+200;
+		r = b = 0;
+	} else if(family === 'blue'){
+		b = Math.floor(Math.random()*55)+200;
+		r = g = 0;
+	} else if(family === 'cyan'){
+		g = b = Math.floor(Math.random()*55)+200;
+		r = 0;
+	} else if(family === 'magenta'){
+		r = b = Math.floor(Math.random()*55)+200;
+		g = 0;
+	} else if(family === 'maroon'){
+		r = Math.floor(Math.random()*28)+100;
+		b = g = 0;
+	} else if(family === 'olive'){
+		r = g = Math.floor(Math.random()*28)+100;
+		b = 0;
+	} else if(family === 'green'){
+		g = Math.floor(Math.random()*28)+100;
+		r = b = 0;
+	} else if(family === 'purple'){
+		r = b = Math.floor(Math.random()*28)+100;
+		g = 0;
+	} else if(family === 'teal'){
+		g = b = Math.floor(Math.random()*28)+100;
+		r = 0;
+	} else if(family === 'navy'){
+		b = Math.floor(Math.random()*28)+100;
+		r = g = 0;
+	} else if(family === 'black'){
+		r = g = b = Math.floor(Math.random()*42);
+	} else if(family === 'white'){
+		r = g = b = Math.floor(Math.random()*35)+220;
+	} else if(family === 'silver'){
+		r = g = b = Math.floor(Math.random()*42)+170;
+	} else if(family === 'grey'){
+		r = g = b = Math.floor(Math.random()*28)+100;
+	}
+	r0 = r/255;
+	g0 = g/255;
+	b0 = b/255;
+	base = RGBToHex(r,g,b);
+}
+
+//Convert RGB to HSL for Color Theory support
+
+//Luminosity
+//Find max and min
+let max = Math.max(r0, g0, b0);
+let min = Math.min(r0, g0, b0);
+let lum = (1 / 2) * (max + min);
+
+//Hue
+let hue;
+if(max === r0){
+	if(min === g0){
+		//R > B > G
+		hue = 60 * (6 * (b0-g0)/(r0-g0));
+	} else {
+		//R > G > B
+		hue = 60 * ((g0-b0)/(r0-b0));
+	}
+} else if(max === g0){
+	if(min === b0){
+		//G > R > B
+		hue = 60 * (2 - (r0-b0)/(g0-b0));
+	} else {
+		//G > B > R
+		hue = 60 * (2 + (b0-r0)/(g0-r0));
+	}
+} else if(max === b0){
+	if(min === r0){
+		//B > G > R
+		hue = 60 * (4 - (g0-r0)/(b0-r0));
+	} else {
+		//B > R > G
+		hue = 60 * (4 + (r0-g0)/(b0-g0));
+	}
+}
+
+//Saturation
+let sat;
+if(lum === 1){
+sat = 0;
+} else {
+sat = ((max - min) / (1 - (lum*2 - 1)));
+}
+
+//Base HSL color... hue, sat, lum
+
+//Complementary
+let complRGB = HSLToRGB(Math.abs((hue + 180) - 360), sat, lum);
+let compl = RGBToHex(complRGB.r, complRGB.g, complRGB.b);
+
+//Split-complementary
+let splitComplRGB = [
+HSLToRGB(Math.abs((hue + 150) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 210) - 360), sat, lum)
+];
+let splitCompl = [
+RGBToHex(splitComplRGB[0].r, splitComplRGB[0].g, splitComplRGB[0].b),
+RGBToHex(splitComplRGB[1].r, splitComplRGB[1].g, splitComplRGB[1].b)
+];
+
+//Triadic
+let triadicRGB = [
+HSLToRGB(Math.abs((hue + 120) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 240) - 360), sat, lum)
+];
+let triadic = [
+RGBToHex(triadicRGB[0].r, triadicRGB[0].g, triadicRGB[0].b),
+RGBToHex(triadicRGB[1].r, triadicRGB[1].g, triadicRGB[1].b)
+];
+
+//Tetradic
+let tetradicRGB = [
+HSLToRGB(Math.abs((hue + 90) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 180) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 270) - 360), sat, lum)
+];
+let tetradic = [
+RGBToHex(tetradicRGB[0].r, tetradicRGB[0].g, tetradicRGB[0].b),
+RGBToHex(tetradicRGB[1].r, tetradicRGB[1].g, tetradicRGB[1].b),
+RGBToHex(tetradicRGB[2].r, tetradicRGB[2].g, tetradicRGB[2].b)
+];
+
+//Analagous
+let analogRGB = [
+HSLToRGB(Math.abs((hue + 30) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 60) - 360), sat, lum),
+HSLToRGB(Math.abs((hue + 90) - 360), sat, lum)
+];
+let analog = [
+RGBToHex(analogRGB[0].r, analogRGB[0].g, analogRGB[0].b),
+RGBToHex(analogRGB[1].r, analogRGB[1].g, analogRGB[1].b),
+RGBToHex(analogRGB[2].r, analogRGB[2].g, analogRGB[2].b)
+];
+
+//Monochrome (25-60% | 42%)
+let mono = [];
+const spread = 0.42;
+let light = RGBToHex(r*(1+spread),g*(1+spread),b*(1+spread));
+mono.push(light);
+let dark = RGBToHex(r*(1-spread),g*(1-spread),b*(1-spread));
+mono.push(dark);
+//red_light = red_primary * (1 + scaling_factor)
+//green_light = green_primary * (1 + scaling_factor)
+//blue_light = blue_primary * (1 + scaling_factor)
+
+//red_dark = red_primary * (1 - scaling_factor)
+//green_dark = green_primary * (1 - scaling_factor)
+//blue_dark = blue_primary * (1 - scaling_factor)
+
+return {base, light, dark, compl, splitCompl, triadic, tetradic, analog};
+
+}
+/*
+let newColor1 = colorTheoryGen();
+let newColor1 = colorTheoryGen('#00d3d3');
+let newColor1 = colorTheoryGen(false, 'red');
+console.log(newColor1.base);
+console.log(newColor1.compl);
+console.log(newColor1.splitCompl[0]);
+console.log(newColor1.splitCompl[1]);
+console.log(newColor1.triadic[0]);
+console.log(newColor1.triadic[1]);
+console.log(newColor1.tetradic[0]);
+console.log(newColor1.tetradic[1]);
+console.log(newColor1.tetradic[2]);
+console.log(newColor1.analog[0]);
+console.log(newColor1.analog[1]);
+console.log(newColor1.analog[2]);
+*/
